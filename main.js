cc431d9dab32eabd79a3e23c653d382aa2db62f9
camachoo18 <estivencamachops@gmail.com>
2024-02-08 09:38:00 +0100

    #Añado mi pokeguess

---
+++ main.js
@@ -0,0 +1,85 @@
+async function getPkmn(numero) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${numero}`;
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    return datos;
+}
+
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * 999) + 1;
+}
+
+async function mi_peticion() {
+    const imagenDesenfocada = document.querySelector('#imagenDesenfocada');
+    imagenDesenfocada.classList.remove('hidden');
+
+    const numeroAleatorio = generarNumeroAleatorio();
+    const numeroAleatorio1 = generarNumeroAleatorio();
+    const numeroAleatorio2 = generarNumeroAleatorio();
+    const numeroAleatorio3 = generarNumeroAleatorio();
+
+    const pkmn = await getPkmn(numeroAleatorio);
+    const pkmn1 = await getPkmn(numeroAleatorio1);
+    const pkmn2 = await getPkmn(numeroAleatorio2);
+    const pkmn3 = await getPkmn(numeroAleatorio3);
+
+    const { front_default } = pkmn.sprites;
+    const urlImagenFrontal = front_default;
+
+    const imgPokemon = document.querySelector("#imgPokemon");
+    imgPokemon.src = urlImagenFrontal;
+
+    imagenDesenfocada.classList.add('hidden');
+
+    mostrarNombrePokemon(pkmn.name, pkmn1.name, pkmn2.name, pkmn3.name);
+
+    const opciones = {
+        win: pkmn.name,
+        lose1: pkmn1.name,
+        lose2: pkmn2.name,
+        lose3: pkmn3.name
+    };
+
+    updateOpciones(opciones);
+}
+
+function updateOpciones(opciones) {
+    const opcion0 = document.querySelector("#opcion0");
+    const opcion1 = document.querySelector("#opcion1");
+    const opcion2 = document.querySelector("#opcion2");
+    const opcion3 = document.querySelector("#opcion3");
+
+    const botones = [opcion0, opcion1, opcion2, opcion3];
+    botones.sort(() => Math.random() - 0.5);
+
+    botones[0].value = opciones.win;
+    botones[1].value = opciones.lose1;
+    botones[2].value = opciones.lose2;
+    botones[3].value = opciones.lose3;
+}
+mi_peticion()
+
+document.addEventListener("DOMContentLoaded", function () {
+    mi_peticion();  
+
+    const obtenerPokemonBotones = document.querySelectorAll("#pkmon, #pkmon1, #pkmon2, #pkmon3");
+
+
+    obtenerPokemonBotones.forEach(function (boton) {
+        boton.addEventListener("click", function () {
+            mi_peticion();
+        });
+    });
+});
+
+function mostrarNombrePokemon(nombrePokemon, nombrePokemon1, nombrePokemon2, nombrePokemon3) {
+    const btnObtenerPokemon = document.querySelector("#pkmon");
+    const btnObtenerPokemon1 = document.querySelector("#pkmon1");
+    const btnObtenerPokemon2 = document.querySelector("#pkmon2");
+    const btnObtenerPokemon3 = document.querySelector("#pkmon3");
+
+    btnObtenerPokemon.value = `${nombrePokemon}`;
+    btnObtenerPokemon1.value = `${nombrePokemon1}`;
+    btnObtenerPokemon2.value = `${nombrePokemon2}`;
+    btnObtenerPokemon3.value = `${nombrePokemon3}`;
+}
